95485a6d1c1e574fe201cd54604006a6814469a0
A-Stanley <astanley@uplandsoftware.com>
2020-10-26 20:07:11 +0100

    access data through provider and added a method to check id in provider

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -44,7 +44,12 @@ class Products with ChangeNotifier {
     return [..._items];
   }
 
+  Product findById(String id) {
+    return _items.firstWhere((prod) => prod.id == id); 
+  }
+
   void addProduct() {
+    // _tems.add(value);
     notifyListeners();
   }
 }
