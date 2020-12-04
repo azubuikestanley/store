bf2aca9c4713cef04365c37dce2b49ea4055e098
A-Stanley <astanley@uplandsoftware.com>
2020-12-04 01:43:07 +0100

    updating the favourite status optimistically

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +20,28 @@ class Product with ChangeNotifier {
     this.isFavourite = false,
   });
 
-  void toggleFavoriteStatus() {
-    isFavourite = !isFavourite;
+  void _setFavValue(bool newValue) {
+    isFavourite = newValue;
     notifyListeners();
   }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavourite;
+    isFavourite = !isFavourite;
+    notifyListeners();
+    final url = 'https://flutter-office-store.firebaseio.com/products/$id.json';
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavourite': isFavourite,
+        }),
+      );
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
+  } 
 }
