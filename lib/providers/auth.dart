96491d138b666fad288ecc29d162b828eda73b2f
A-Stanley <astanley@uplandsoftware.com>
2020-12-08 01:18:29 +0100

    handled auth error response

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -4,6 +4,8 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
@@ -15,17 +17,24 @@ class Auth with ChangeNotifier {
 
     final url =
         'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$acKey';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'retureSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'retureSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
   }
 
   Future<void> signup(String email, String password) async {
